3fa3db1a64aa386c44a8763ea87b8161632c85cd
yuwei2 <yuwei2@vipkid.com.cn>
2019-12-20 20:12:37 +0800

    reload

--- src/main/java/com/ckp/test/abstractfactory/ColorCreator.java
+++ src/main/java/com/ckp/test/abstractfactory/ColorCreator.java
@@ -2,6 +2,7 @@ package com.ckp.test.abstractfactory;
 
 public class ColorCreator extends AbstractCreator {
 
+    @Override
     public Color getColor(String name) {
         if("ConcreteColor".equals(name)){
             return new ConcreteColor();
@@ -9,6 +10,7 @@ public class ColorCreator extends AbstractCreator {
         return null;
     }
 
+    @Override
     public Shape getShape(String name) {
         return null;
     }
--- src/main/java/com/ckp/test/abstractfactory/ShapeCreator.java
+++ src/main/java/com/ckp/test/abstractfactory/ShapeCreator.java
@@ -2,10 +2,12 @@ package com.ckp.test.abstractfactory;
 
 public class ShapeCreator extends AbstractCreator {
 
+    @Override
     public Color getColor(String name) {
         return null;
     }
 
+    @Override
     public Shape getShape(String name) {
         if("ConcreteShape".equals(name)){
             return new ConcreteShape();
